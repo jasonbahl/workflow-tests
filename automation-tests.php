a21937f86eb5f88f6fd1f5df5d606b7fc1a1e805
jasonbahl <jasonbahl@users.noreply.github.com>
2025-08-01 00:22:22 +0000

    release: prepare v5.2.9

--- automation-tests.php
+++ automation-tests.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Automation Tests
  * Description: A plugin to test automation workflows. 
- * Version: 5.2.8
+ * Version: 5.2.9
  * Author: Jason Bahl
  * Tested up to: 6.2
  */
